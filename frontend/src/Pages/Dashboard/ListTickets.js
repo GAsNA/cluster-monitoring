62c3230c321e67a7f46b1b9e6d8495aa8804b3df
GAsNa <romane.leseur.pro@gmail.com>
2023-09-12 23:52:54 +0200

    frontend: date formated in ticket

--- frontend/src/Pages/Dashboard/ListTickets.js
+++ frontend/src/Pages/Dashboard/ListTickets.js
@@ -3,13 +3,19 @@ import { Card, CardBody } from '@nextui-org/react';
 import Ticket from './Ticket.js';
 
 function ListTickets({ tickets, issueTypes }) {
+
+	function getDateFormated(dateStr) {
+		var date = new Date(dateStr)
+		return (date.getDate() + "/" + parseInt(date.getMonth() + 1) + "/" + date.getFullYear())
+	}
+	
 	return (
 		<>
 		{
 			tickets ?
 				tickets.map((ticket, index) => (
 					<div key={index}>
-						<Ticket date="11/09/2023"
+						<Ticket date={getDateFormated(ticket.CreatedAt)}
 								type={(issueTypes.find((it) => ticket.Type === it.ID )).Name} 
 								resolved={ticket.Resolved}
 								comment={ticket.Comment} />
