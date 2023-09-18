c788665dc9d04bc25cfed13aa5e136eec6471243
GAsNa <romane.leseur.pro@gmail.com>
2023-09-18 15:59:50 +0200

    backend: functions generatejwt and verifyjwt

--- backend/go.mod
+++ backend/go.mod
@@ -3,12 +3,16 @@ module main
 go 1.20
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.0.0
+	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.9
+	github.com/uptrace/bun v1.1.14
+	github.com/uptrace/bun/dialect/sqlitedialect v1.1.14
+)
+
+require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/lib/pq v1.10.9 // indirect
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
-	github.com/uptrace/bun v1.1.14 // indirect
-	github.com/uptrace/bun/dialect/sqlitedialect v1.1.14 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
--- backend/jwt/jwt.go
+++ backend/jwt/jwt.go
@@ -1,20 +1,48 @@
 package jwt
 
 import (
-	"golang-jwt"
 	"os"
+	"errors"
+	"time"
 
 	"main/models"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
-func generateJWT(user *models.User) (string, error) {
-	token := jwt.New(jwt.SigningMethodEdDSA)
+type Claims struct {
+	User	*models.User	`json:"user"`
+	jwt.RegisteredClaims
+}
 
-	claims := token.Claims.(jwt.MapClaims)
-	claims["user"] = user
+var jwtKey = []byte(os.Getenv("JWT_KEY"))
 
-	tokenString, err := token.SignedString(os.Getenv("JWT_KEY"))
+func GenerateJWT(user *models.User) (string, error) {
+	expirationTime := time.Now().Add(7 * 24 * time.Hour)
+	
+	claims := &Claims{
+		User: user,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
 	if err != nil { return "", err }
 
 	return tokenString, nil
+}
+
+func VerifyJWT(tknStr string) (*Claims, error) {
+	claims := &Claims{}
+
+	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (any, error) {
+		return jwtKey, nil
+	})
+	if err != nil { return (*Claims)(nil), err }
+	
+	if !tkn.Valid { return (*Claims)(nil), errors.New("Token invalid") }
+
+	return claims, nil
 }
