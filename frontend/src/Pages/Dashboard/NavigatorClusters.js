5bc3bdf07b38fa961bf0e65fa92cb2013e36e460
GAsNa <romane.leseur.pro@gmail.com>
2023-09-27 17:25:43 +0200

    frontend: navigator of clusters is responsive

--- frontend/src/Pages/Dashboard/NavigatorClusters.js
+++ frontend/src/Pages/Dashboard/NavigatorClusters.js
@@ -3,11 +3,15 @@ import { Navbar, NavbarContent, NavbarItem, Button } from '@nextui-org/react';
 
 function NavigatorClusters({ allClusters, changeCluster }) {
 	return (
-			<Navbar maxWidth="full" isBordered>
-				<NavbarContent className="sm:flex gap-4">
+			<Navbar maxWidth="full" isBordered height="100%">
+				<NavbarContent className="sm:flex gap-4" style={{ display: 'flex', flexWrap: 'wrap', paddingBottom: '10px' }}>
 					{allClusters.map((item) => (
 						<NavbarItem isActive={item.IsActive} key={item.Name}>
-							<Button value={item.ID} onPress={e => changeCluster(e.target.value)} color={item.IsActive && "primary"}>{item.Name}</Button>
+							<Button value={item.ID} onPress={e => changeCluster(e.target.value)}
+								color={item.IsActive && "primary"}
+							>
+								{item.Name}
+							</Button>
 						</NavbarItem>
 					))}
 				</NavbarContent>
