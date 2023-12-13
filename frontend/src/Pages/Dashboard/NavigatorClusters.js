cc73049fd91a8c7e95e1c98405b47340ed3ae06b
GAsNa <romane.leseur.pro@gmail.com>
2023-12-13 15:33:53 +0100

    frontend: dashboard: fix top padding on nav clusters

--- frontend/src/Pages/Dashboard/NavigatorClusters.js
+++ frontend/src/Pages/Dashboard/NavigatorClusters.js
@@ -4,7 +4,7 @@ import { Navbar, NavbarContent, NavbarItem, Button } from '@nextui-org/react';
 function NavigatorClusters({ allClusters, changeCluster }) {
 	return (
 			<Navbar maxWidth="full" isBordered height="100%">
-				<NavbarContent className="sm:flex gap-4" style={{ display: 'flex', flexWrap: 'wrap', paddingBottom: '10px' }}>
+				<NavbarContent className="sm:flex gap-4" style={{ display: 'flex', flexWrap: 'wrap', padding: '10px 0' }}>
 					{allClusters.map((item) => (
 						<NavbarItem isActive={item.IsActive} key={item.Name}>
 							<Button value={item.ID} onPress={e => changeCluster(e.target.value)}
